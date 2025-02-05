09e9ae62377fffa0eed1000eb0736f8999859c9a
massmr <marcellin.massimo@gmail.com>
2025-02-05 15:08:48 +0100

    refactor : correct port

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use('/api', jobsRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
