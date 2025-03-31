9a75e30d879dbf58ad6dd6eb68edbe8f325f23c8
massmr <marcellin.massimo@gmail.com>
2025-03-31 13:58:07 +0200

    feat : add deletion in mongodb list

--- services/apiService.js
+++ services/apiService.js
@@ -1,9 +1,10 @@
 /*
  * Name : apiService.js
- * Dev : Maxance
+ * Dev : Maxance, Massimo
  * Feature : This service will manage all interactions with jobposting's API.
+ * description : It gets all the offers from jobpostingpro => then write or delete it from mongodb
  * how : 
- * Version : 1.0
+ * Version : 1.2 => deletion of obsolete offers
  */
 
 const Job = require('../models/jobOffer'); 
@@ -16,9 +17,29 @@ exports.fetchData = async function () {
 
     try {
         const response = await axios.get(url);
+        // get all job offers from API
+        if (!response || !response.data) {
+            throw new Error("Aucune donnée reçue de l'API.");
+        }
         const jobDataArray = response.data;
-
         console.log(`${jobDataArray.length} offres récupérées.`);
+
+        // Get all id of jon offers from the API
+        const apijobsId = jobDataArray.map(job => job.offre_id);
+
+        // Get all ids of job offers already stored in the database
+        const existingJobsId = await Job.find({}, 'offre_id').then(jobs => jobs.map(job => job.offre_id));
+
+        // TODO : test
+        // Delete jobs that are no longer in the API
+        const obsoleteJobsId = [...existingJobsId].filter(id => !apiJobsId.has(id));
+        if (obsoleteJobsId.length > 0) {
+            await Job.deleteMany({ offre_id: { $in: obsoleteJobsId } });
+            console.log(`${obsoleteJobsId.length} offres obsolètes supprimées de MongoDB.`);
+        } else {
+            console.log("Aucune offre obsolète à supprimer.");
+        }
+
 
         for (const jobData of jobDataArray) {
             try {
